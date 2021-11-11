9205a8de4274cfb3a963d544ba13fac9348de611
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-11 13:45:53 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    shake = 1
+})
+let shake = 0
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-    	
+        shake = 1
+        while (shake) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+        }
     }
 })
